ce59a5a61e2e4b76a86957079663308e16558825
adam-st <adam.stankiewicz.g4@gmail.com>
2023-03-01 10:58:43 +0100

    modyfikacja pliku

---
+++ 1_podstawy.php
@@ -0,0 +1,15 @@
+<?php
+  $firstName = "Janusz";
+  $lastName = "Nowak";
+  echo "Imię i nazwisko: $firstName $lastName<br>";
+  echo 'Imię i nazwisko: $firstName $lastName<br>';
+
+  //heredoc
+  echo <<< DATA
+    <hr>
+    Imię: $firstName<br>
+    Nazwisko: $lastName<br>
+DATA;
+
+
+?>
